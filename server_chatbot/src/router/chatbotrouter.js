095214e73b6b3212752579574a674cdb86df1fa0
JIJIN12 <jijinsuresh6@gmail.com>
2023-12-18 13:32:27 +0530

    Update chatbotrouter.js

--- server_chatbot/src/router/chatbotrouter.js
+++ server_chatbot/src/router/chatbotrouter.js
@@ -1,58 +1,56 @@
-const express = require('express')
-const chatbotmodel = require('../model/chatbotmodel');
-const { OpenAI } = require('openai');
-const chatbotRouter = express.Router()
+const express = require("express");
+const chatbotmodel = require("../model/chatbotmodel");
+const { OpenAI } = require("openai");
+const chatbotRouter = express.Router();
 
-chatbotRouter.post('/chat', async function (req, res) {
-    console.log(req.body);
-    try {
-        const openai = new OpenAI({
-            apiKey: "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"
-        });
-        const data = {
-            role: 'user',
-            text: req.body.text
-        }
-        const messages = await chatbotmodel(data).save()
+chatbotRouter.post("/chat", async function (req, res) {
+  console.log("body", req.body);
+  try {
+    const openai = new OpenAI({
+      apiKey: "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",
+    });
+    const data = {
+      role: "user",
+      text: req.body.text,
+    };
 
-        const response = await openai.chat.completions.create({
-            model: 'gpt-3.5-turbo',
-            messages: [{ role: 'user', content: req.body.text }],
-            max_tokens: 100
-        });
-        const responsechat = response.choices[0].message
-        console.log(responsechat);
-        const data1 = {
-            role: response.choices[0].message.role,
-            text: response.choices[0].message.content
-        }
-        const message = await chatbotmodel(data1).save()
+    const messages = await chatbotmodel(data).save();
+    console.log("1");
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: req.body.text }],
+      max_tokens: 100,
+    });
+    console.log("2");
 
-        res.status(200).json(response.choices[0].message); // Send the response to the client
-    } catch (error) {
-        console.error(error);
-        res.status(400).json({ success: false, error: true, message: "Something went wrong" });
-    }
+    const responsechat = response.choices[0].message;
+    console.log(responsechat);
+    const data1 = {
+      role: response.choices[0].message.role,
+      text: response.choices[0].message.content,
+    };
+    const message = await chatbotmodel(data1).save();
+    console.log(message);
 
+    res.status(200).json(response.choices[0].message); // Send the response to the client
+  } catch (error) {
+    console.error(error);
+    res
+      .status(400)
+      .json({ success: false, error: true, message: "Something went wrong" });
+  }
+});
+chatbotRouter.get("/view-chat", async function (req, res) {
+  try {
+    const messages = await chatbotmodel.find();
 
+    res.status(200).json(messages); // Send the response to the client
+  } catch (error) {
+    console.error(error);
+    res
+      .status(400)
+      .json({ success: false, error: true, message: "Something went wrong" });
+  }
+});
 
-})
-chatbotRouter.get('/view-chat', async function (req, res) {
- 
-    try {
-        
-        const messages = await chatbotmodel.find()
-
-    
-
-        res.status(200).json(messages); // Send the response to the client
-    } catch (error) {
-        console.error(error);
-        res.status(400).json({ success: false, error: true, message: "Something went wrong" });
-    }
-
-
-
-})
-
-module.exports = chatbotRouter
+module.exports = chatbotRouter;
